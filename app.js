e8f7477674238c0428cdf8a7e8f75976398528e3
Adam Sunderland <iterion@gmail.com>
2010-10-19 16:37:50 +0000

    Making routes nicer

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.get('/', function(req, res) {
 //where [field] is a field like artist or album
 //and [search] is the actual string for which the user 
 //has searched
-app.get('/find/*', function(req, res) {
-	var tag = req.params[0];
+app.get('/find/:tag', function(req, res) {
+	var tag = req.params.id;
 	console.log('search for tag: ' + tag);
 	filesProvider.findByTag(tag, function(err, docs) {
 		res.render('files', {
@@ -39,16 +39,16 @@ app.get('/find/*', function(req, res) {
 });
 
 //download link - not used for streaming
-app.get('/download/*', function(req, res) {
-	filesProvider.findById(req.params[0], function(error, file) {
+app.get('/download/:id', function(req, res) {
+	filesProvider.findById(req.params.id, function(error, file) {
 		console.log("download:" + file.name);
 		res.download(file.path + "/" + file.name);
 	});
 });
 
 //like download - but used for streaming
-app.get('/stream/*', function(req, res) {
-	filesProvider.findById(req.params[0], function(error, file) {
+app.get('/stream/:id', function(req, res) {
+	filesProvider.findById(req.params.id, function(error, file) {
 		console.log("stream:" + file.name);
 		res.sendfile(file.path + "/" + file.name);
 	});
@@ -57,8 +57,8 @@ app.get('/stream/*', function(req, res) {
 
 //view the file - view automaticall determines type and
 //sets up viewer
-app.get('/view/*', function(req, res) {
-	filesProvider.findById(req.params[0], function(error, file) {
+app.get('/view/:id', function(req, res) {
+	filesProvider.findById(req.params.id, function(error, file) {
 		console.log("view:" + file.name);
 		res.render('stream', {
 			locals: {
